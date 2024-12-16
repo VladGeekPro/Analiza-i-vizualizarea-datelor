9b5c60fd41f152f8e447038f02ba171a39f67e74
JuracovschiVladislav <126159146+VladGeekPro@users.noreply.github.com>
2024-12-16 11:11:17 +0200

    Add files via upload

---
+++ Lab 3/APIGetData/GetDioxideCarbon.py
@@ -0,0 +1,31 @@
+import cdsapi
+
+dataset = "cams-global-radiative-forcings"
+request = {
+    "variable": ["radiative_forcing_of_carbon_dioxide"],
+    "forcing_type": "instantaneous",
+    "band": [
+        "long_wave",
+        "short_wave"
+    ],
+    "sky_type": ["all_sky"],
+    "level": ["surface"],
+    "version": ["2"],
+    "year": [
+        "2003", "2004", "2005",
+        "2006", "2007", "2008",
+        "2009", "2010", "2011",
+        "2012", "2013", "2014",
+        "2015", "2016", "2017",
+        "2018"
+    ],
+    "month": [
+        "01", "02", "03",
+        "04", "05", "06",
+        "07", "08", "09",
+        "10", "11", "12"
+    ]
+}
+
+client = cdsapi.Client()
+client.retrieve(dataset, request).download()
